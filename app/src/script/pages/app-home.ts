57819fea0c3a7d6abf6df1290ce5bfe4fe3e2f8b
Zach Teutsch <88554871+zateutsch@users.noreply.github.com>
2022-07-22 10:36:56 -0700

    Restructure

---
+++ app/src/script/pages/app-home.ts
@@ -0,0 +1,79 @@
+import { LitElement, css, html } from 'lit';
+import { property, customElement } from 'lit/decorators.js';
+
+// For more info on the @pwabuilder/pwainstall component click here https://github.com/pwa-builder/pwa-install
+import '@pwabuilder/pwainstall';
+
+@customElement('app-home')
+export class AppHome extends LitElement {
+
+  // For more information on using properties and state in lit
+  // check out this link https://lit.dev/docs/components/properties/
+  @property() message = 'Welcome!';
+
+  static get styles() {
+    return css`
+      #welcomeBar {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      }
+
+      #welcomeBar fluent-card {
+        margin-bottom: 12px;
+      }
+
+
+
+      h1 {
+        font-family: "bubble";
+        font-size: 100px;
+        color: white;
+      }
+
+
+      @media (horizontal-viewport-segments: 2) {
+        #welcomeBar {
+          flex-direction: row;
+          align-items: flex-start;
+          justify-content: space-between;
+        }
+
+      }
+
+    `;
+  }
+
+  constructor() {
+    super();
+  }
+
+  async firstUpdated() {
+    // this method is a lifecycle even in lit
+    // for more info check out the lit docs https://lit.dev/docs/components/lifecycle/
+    console.log('This is your home page');
+  }
+
+  share() {
+    if ((navigator as any).share) {
+      (navigator as any).share({
+        title: 'PWABuilder pwa-starter',
+        text: 'Check out the PWABuilder pwa-starter!',
+        url: 'https://github.com/pwa-builder/pwa-starter',
+      });
+    }
+  }
+
+  render() {
+    return html`
+      <div>
+        <div id="welcomeBar">
+          <h1>
+            CopyRoom
+          </h1>
+        </div>
+      </div>
+    `;
+  }
+}
